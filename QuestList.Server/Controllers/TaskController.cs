50f89076c52b5ecf7619a5483429bf2c62dd9c18
Clayton Hunt <claytonhunt_104@hotmail.com>
2019-03-15 14:37:39 -0400

    Added Task Repository

---
+++ QuestList.Server/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuestList.Shared.Interfaces;
+using QuestList.Shared.Models;
+
+namespace QuestList.Server.Controllers
+{
+    [ApiController]
+    [Route("quests/{questId}/tasks")]
+    public class TaskController : Controller
+    {
+        private readonly IRepository<QuestTask> _taskRepository;
+
+        public TaskController(IRepository<QuestTask> taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [HttpGet("{taskId?}")]
+        public async Task<IList<QuestTask>> GetTasks(int questId, int? taskId)
+        {
+            return await _taskRepository.ReadAll(t => t.Quest.Id == questId && (taskId == null || taskId == t.Id));
+        }
+    }
+}
