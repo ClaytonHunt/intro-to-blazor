5d64628c1bc7ad66694d7110d8418c412165cfb1
Clayton Hunt <claytonhunt_104@hotmail.com>
2019-03-15 13:54:43 -0400

    Added quests controller and modified startup

---
+++ QuestList.Data/DataStartup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using QuestList.Data.Repositories;
+using QuestList.Shared.Interfaces;
+using QuestList.Shared.Models;
+
+namespace QuestList.Data
+{
+    public static class DataStartup
+    {
+        public static IServiceCollection AddQuestData(this IServiceCollection services)
+        {
+            services.AddDbContext<QuestLineContext>(options => options.UseSqlite("Data Source=quests.db"));
+
+            services.AddScoped<IRepository<QuestLine>, QuestRepository>();
+
+            return services;
+        }
+
+        public static IServiceScopeFactory EnsureQuestData(this IServiceScopeFactory scopeFactory)
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<QuestLineContext>();
+
+                if (db.Database.EnsureCreated())
+                {
+                    SeedData.Initialize(db);
+                }
+            }
+
+            return scopeFactory;
+        }
+    }
+}
---
+++ QuestList.Server/Controllers/QuestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuestList.Shared.Interfaces;
+using QuestList.Shared.Models;
+
+namespace QuestList.Server.Controllers
+{
+    [ApiController]
+    [Route("quests")]
+    public class QuestController : Controller
+    {
+        private readonly IRepository<QuestLine> _questRepository;
+
+        public QuestController(IRepository<QuestLine> questRepository)
+        {
+            _questRepository = questRepository;
+        }
+
+        [HttpGet("{id?}")]
+        public async Task<IList<QuestLine>> GetQuests(int? id = null)
+        {
+            Func<QuestLine, bool> predicate = ql => true;
+
+            if (id != null)
+            {
+                predicate = ql => ql.Id == id;
+            }
+
+            return await _questRepository.ReadAll(predicate);
+        }
+    }
+}
