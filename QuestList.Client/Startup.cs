60d0d655c40f69cd9c98aef5e75244eec7750e7b
Clayton Hunt <claytonhunt_104@hotmail.com>
2019-03-17 04:08:21 -0400

    Added stateful event binding to index

--- QuestList.Client/Startup.cs
+++ QuestList.Client/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using QuestList.Client.State;
 
 namespace QuestList.Client
 {
@@ -7,6 +8,7 @@ namespace QuestList.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<QuestState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
