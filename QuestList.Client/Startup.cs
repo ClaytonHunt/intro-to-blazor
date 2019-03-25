5d025985d34166067e97711befca5dd29141b60c
Clayton Hunt <claytonhunt_104@hotmail.com>
2019-03-25 04:54:06 -0400

    extracted shared components to component library

--- QuestList.Client/Startup.cs
+++ QuestList.Client/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using QuestList.Client.State;
+using QuestList.ComponentLibrary.State;
 
 namespace QuestList.Client
 {
